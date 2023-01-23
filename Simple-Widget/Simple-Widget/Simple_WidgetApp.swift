5ddf4711e645880fa411c294ce3c8c2194d90e70
Song Lee <songlish233@gmail.com>
2023-01-23 18:06:11 +0900

    기본 위젯 ui 생성

---
+++ Simple-Widget/Simple-Widget/Simple_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_WidgetApp.swift
+//  Simple-Widget
+//
+//  Created by song-lee on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple-Widget/SimpleWidget/SimpleWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetBundle.swift
+//  SimpleWidget
+//
+//  Created by song-lee on 2023/01/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SimpleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SimpleWidget()
+        SimpleWidgetLiveActivity()
+    }
+}
